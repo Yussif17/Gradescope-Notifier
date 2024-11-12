955fcbea5205e5c9ce1f5eff2decaec465ae6cf7
ahmad8743 <hamzehahmad06@gmail.com>
2024-11-11 22:22:10 -0600

    working

--- loggedInScraper.py
+++ loggedInScraper.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-#from alert_user import alert_user
+from alert_user import alert_user
 
 def goToClass(driver, numAssignments, assignmentStatus):
   courseBox = driver.find_element(By.XPATH, "//*[text()='Math 203']")
