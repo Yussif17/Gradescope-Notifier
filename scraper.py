74c09997b15dc25f560b699644c1b247a5edc29e
Yussif17 <r.yussif@wustl.edu>
2024-11-12 16:49:05 -0600

    checks both math and 247

--- scraper.py
+++ scraper.py
@@ -4,13 +4,16 @@ from creds import email, password, path, databaseURL
 from loggedInScraper import goToClass
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
+import undetected_chromedriver.v2 as uc
 
 
 def login(e, p):
     url = 'https://www.gradescope.com/'
     chrome_options = Options()
     chrome_options.add_argument("--headless") 
-    browser = webdriver.Chrome(options=chrome_options)
+    #browser = webdriver.Chrome(options=chrome_options)
+    browser = uc.Chrome(headless=True)
+    
     browser.get(url)
     init_button = browser.find_element(By.CSS_SELECTOR, "[class='tiiBtn tiiBtn-secondarySplash js-logInButton']")    
     init_button.click()
@@ -22,7 +25,8 @@ def login(e, p):
     password_box.send_keys(p)
     form = browser.find_element(By.CSS_SELECTOR, "form")
     form.submit()
-    goToClass(browser)
+    goToClass(browser, "Math 203")
+    goToClass(browser, "CSE 247/502N")
 
 
 login(email, password)
