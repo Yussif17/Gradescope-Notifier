44544b4d406830a0dd752a1bfd6fbbb8b1ad1e22
Yussif17 <r.yussif@wustl.edu>
2024-11-11 22:49:17 -0600

    both emails

--- alert_user.py
+++ alert_user.py
@@ -1,7 +1,8 @@
 import yagmail
-from creds import google_email, google_password
+from creds import google_email, google_password, reciever2_email, reciever_email
 
 def alert_user():
     yag = yagmail.SMTP(google_email, google_password)
-    yag.send('yussiframa10@gmail.com', 'Hello sir', 'Good day')
+    yag.send(reciever_email, 'GET HYPE', 'NEW ASSIGNMENT OR GRADE DROPPED')
+    yag.send(reciever2_email, 'GET HYPE', 'NEW ASSIGNMENT OR GRADE DROPPED')
     print("Email sent successfully!")
