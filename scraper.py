25bafc8f134df219d7cecc47ac043ec4fc865c47
ahmad8743 <hamzehahmad06@gmail.com>
2024-11-11 21:04:16 -0600

    Remove creds.py from tracking

---
+++ scraper.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from creds import email, password
+
+
+def login(e, p):
+    url = 'https://www.gradescope.com/'
+    browser = webdriver.Chrome()
+    browser.get(url)
+    init_button = browser.find_element(By.CSS_SELECTOR, "[class='tiiBtn tiiBtn-secondarySplash js-logInButton']")    
+    init_button.click()
+
+    email_box = browser.find_element(By.ID, "session_email")
+    password_box = browser.find_element(By.ID, "session_password")
+
+    email_box.send_keys(e)
+    password_box.send_keys(p)
+    form = browser.find_element(By.CSS_SELECTOR, "form")
+    form.submit()
+
+login(email, password)
